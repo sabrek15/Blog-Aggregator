70f3364d1ea677409cb2b1ca0a8f5303f315bfe0
sabrek15 <reddy.ksb007@gmail.com>
2025-03-26 15:38:53 +0530

    added config struct of .gatorconfig.json to connect database

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -10,62 +9,62 @@ import (
 const configFileName = ".gatorconfig.json"
 
 type Config struct {
-	DBURL	string `json:"db_url"`
-	CurrentUserName		string `json:"current_user_name"`
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func (cfg *Config) SetUser(userName string) error {
+	cfg.CurrentUserName = userName
+	return write(*cfg)
 }
 
 func Read() (Config, error) {
-	filePath, err := getConfigFilePath();
+	fullPath, err := getConfigFilePath()
 	if err != nil {
 		return Config{}, err
 	}
 
-	resp, err := os.Open(filePath)
+	file, err := os.Open(fullPath)
 	if err != nil {
-		return Config{}, fmt.Errorf("failed to open config file: %w", err)
+		return Config{}, err
 	}
-	defer resp.Close()
+	defer file.Close()
 
-	var cfg Config
-
-	err = json.NewDecoder(resp).Decode(&cfg)
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	err = decoder.Decode(&cfg)
 	if err != nil {
-		return Config{}, fmt.Errorf("failed to decode JSON: %w", err)
+		return Config{}, err
 	}
 
 	return cfg, nil
 }
 
 func getConfigFilePath() (string, error) {
-	homeDir, err := os.UserHomeDir();
+	home, err := os.UserHomeDir()
 	if err != nil {
-		return "", fmt.Errorf("failed to get home directory: %w", err)
+		return "", err
 	}
-
-	return filepath.Join(homeDir, configFileName), nil
+	fullPath := filepath.Join(home, configFileName)
+	return fullPath, nil
 }
 
-func (cfg *Config) SetUser(userName string) error {
-	cfg.CurrentUserName = userName
-	return Write(*cfg)
-}
-
-func Write(cfg Config) error {
-	filePath, err := getConfigFilePath()
+func write(cfg Config) error {
+	fullPath, err := getConfigFilePath()
 	if err != nil {
-		return fmt.Errorf("failed to get file path: %w", err)
+		return err
 	}
 
-	file, err := os.Create(filePath)
+	file, err := os.Create(fullPath)
 	if err != nil {
-		return fmt.Errorf("failed to create file: %w", err)
+		return err
 	}
 	defer file.Close()
 
-	encoder:= json.NewEncoder(file)
-
-	if err := encoder.Encode(cfg); err != nil {
-		return fmt.Errorf("failed to encode json: %w", err)
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(cfg)
+	if err != nil {
+		return err
 	}
 
 	return nil
