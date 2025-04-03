b2d17e0431dcbe10cddd79ce40fcf47a90355046
sabrek15 <reddy.ksb007@gmail.com>
2025-04-04 01:30:09 +0530

    registered new commands like  and  in main file

--- main.go
+++ main.go
@@ -44,6 +44,8 @@ func main() {
 	cmds.register("agg", handlerAgg)
 	cmds.register("addfeed", handlerAddfeed)
 	cmds.register("feeds", handlerFeeds)
+	cmds.register("follow", handlerFollow)
+	cmds.register("following", hanlderFollowing)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
