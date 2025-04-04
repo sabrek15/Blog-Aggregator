577fd7e2f5115e3f85e9c1a72bdf313864f995b1
sabrek15 <reddy.ksb007@gmail.com>
2025-04-04 15:55:38 +0530

    updated the database

--- internal/database/feed_follows.sql.go
+++ internal/database/feed_follows.sql.go
@@ -72,6 +72,21 @@ func (q *Queries) CreateFeedFollow(ctx context.Context, arg CreateFeedFollowPara
 	return i, err
 }
 
+const deteleFeedFollow = `-- name: DeteleFeedFollow :exec
+DELETE FROM feed_follows
+WHERE feed_id = $1 AND user_id = $2
+`
+
+type DeteleFeedFollowParams struct {
+	FeedID uuid.UUID
+	UserID uuid.UUID
+}
+
+func (q *Queries) DeteleFeedFollow(ctx context.Context, arg DeteleFeedFollowParams) error {
+	_, err := q.db.ExecContext(ctx, deteleFeedFollow, arg.FeedID, arg.UserID)
+	return err
+}
+
 const getFeedFollowsForUser = `-- name: GetFeedFollowsForUser :many
 SELECT feed_follows.id, feed_follows.created_at, feed_follows.updated_at, feed_follows.user_id, feed_follows.feed_id,
     (
